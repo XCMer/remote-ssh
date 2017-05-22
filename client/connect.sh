4db1c8636b21467c0df2a786d8d586a58372b7c3
Raahul Seshadri <raahul@musictypes.com>
2017-05-22 22:03:35 +0530

    Fixes.

--- client/connect.sh
+++ client/connect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . ./../vars.sh
-ssh -p ${SERVER_SSH_PORT} raahul@${SERVER_IP}
+ssh -p ${SERVER_SSH_PORT}  ${DESKTOP_USERNAME}@${SERVER_IP}
--- desktop/connect.sh
+++ desktop/connect.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 . ./../vars.sh
-autossh -M ${AUTO_SSH_PORT} -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R ${SERVER_IP}:${SERVER_SSH_PORT}:${SERVER_USERNAME}:22 ${SERVER_USERNAME}@${SERVER_IP}
+autossh -M ${AUTO_SSH_PORT} -fN -o "PubkeyAuthentication=yes" -o "StrictHostKeyChecking=false" -o "PasswordAuthentication=no" -o "ServerAliveInterval 60" -o "ServerAliveCountMax 3" -R ${SERVER_IP}:${SERVER_SSH_PORT}:localhost:22 ${SERVER_USERNAME}@${SERVER_IP}
